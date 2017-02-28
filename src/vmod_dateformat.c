979ab8304e4a0e99e177f382238a2bfde053f105
Jonathan Huot <jonathan.huot@thomsonreuters.com>
2017-02-28 18:55:33 +0100

    Added initial version including date2time and time2date.

--- src/vmod_dateformat.c
+++ src/vmod_dateformat.c
@@ -1,7 +1,34 @@
+/*
+ * Copyright 2017 Thomson Reuters
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include "config.h"
 
-#include <stdio.h>
-#include <stdlib.h>
+#define _XOPEN_SOURCE /* glibc2 needs this */
+#include <time.h>
 
 /* need vcl.h before vrt.h for vmod_evet_f typedef */
 #include "vcl.h"
@@ -11,77 +38,57 @@
 #include "vtim.h"
 #include "vcc_dateformat_if.h"
 
-const size_t infosz = 64;
-char	     *info;
-
-/*
- * handle vmod internal state, vmod init/fini and/or varnish callback
- * (un)registration here.
- *
- * malloc'ing the info buffer is only indended as a demonstration, for any
- * real-world vmod, a fixed-sized buffer should be a global variable
- */
-
-int __match_proto__(vmod_event_f)
-event_function(VRT_CTX, struct vmod_priv *priv, enum vcl_event_e e)
+VCL_STRING __match_proto__(td_dateformat_time2date)
+vmod_time2date(VRT_CTX, VCL_STRING format, VCL_TIME time)
 {
-	char	   ts[VTIM_FORMAT_SIZE];
-	const char *event = NULL;
-
-	(void) ctx;
-	(void) priv;
-
-	switch (e) {
-	case VCL_EVENT_LOAD:
-		info = malloc(infosz);
-		if (! info)
-			return (-1);
-		event = "loaded";
-		break;
-	case VCL_EVENT_WARM:
-		event = "warmed";
-		break;
-	case VCL_EVENT_COLD:
-		event = "cooled";
-		break;
-	case VCL_EVENT_DISCARD:
-		free(info);
-		return (0);
-		break;
-	default:
-		return (0);
-	}
-	AN(event);
-	VTIM_format(VTIM_real(), ts);
-	snprintf(info, infosz, "vmod_dateformat %s at %s", event, ts);
-
-	return (0);
-}
-
-VCL_STRING
-vmod_info(VRT_CTX)
-{
-	(void) ctx;
-
-	return (info);
-}
-
-VCL_STRING
-vmod_hello(VRT_CTX, VCL_STRING name)
-{
+	struct tm tm;
+	time_t tt;
+	size_t max, size;
 	char *p;
-	unsigned u, v;
 
-	u = WS_Reserve(ctx->ws, 0); /* Reserve some work space */
-	p = ctx->ws->f;		/* Front of workspace area */
-	v = snprintf(p, u, "Hello, %s", name);
-	v++;
-	if (v > u) {
-		/* No space, reset and leave */
-		WS_Release(ctx->ws, 0);
-		return (NULL);
+	if (format == NULL) {
+		VSLb(ctx->vsl, SLT_Error, "date2str: bad arguments");
+		return "";
 	}
-	/* Update work space with what we've used */
-	WS_Release(ctx->ws, v);
-	return (p);
+
+        tt = (time_t)time;
+        (void)gmtime_r(&tt, &tm);
+
+	max = WS_Reserve(ctx->ws, 0);
+	if (max == 0) {
+		VSLb(ctx->vsl, SLT_Error, "date2str: workspace is empty");
+		return "";
+	}
+	p = ctx->ws->f;
+	size = strftime(p, max, format, &tm);
+	if (size == 0) {
+		WS_ReleaseP(ctx->ws, p);
+		VSLb(ctx->vsl, SLT_Error, "strftime: <%ld> in <%s> format doesn't fit in <%ld> bytes", tt, format, max);
+		return NULL;
+	}
+	WS_Release(ctx->ws, size + 1);
+	return p;
+}
+
+VCL_TIME __match_proto__(td_dateformat_date2time)
+vmod_date2time(VRT_CTX, VCL_STRING format, VCL_STRING str, VCL_TIME fallback)
+{
+	time_t time;
+	struct tm tm;
+
+	if (format == NULL || str == NULL) {
+		VSLb(ctx->vsl, SLT_Error, "str2date: bad arguments");
+		return fallback;
+	}
+
+	if (strptime(str, format, &tm) == NULL) {
+		VSLb(ctx->vsl, SLT_Error, "strptime failed: <%s> is not in <%s> format", str, format);
+		return fallback;
+	}
+	time = mktime(&tm);
+	if (time == -1) {
+		VSLb(ctx->vsl, SLT_Error, "mktime failed: <%s> is not in <%s> format", str, format);
+		return fallback;
+	}
+	return time;
 }
